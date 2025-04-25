e8ae06e46996b8050a7355b4ad72e026eca44bc1
bhumi3046 <143391198+bhumi3046@users.noreply.github.com>
2025-04-25 23:07:25 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_dUH7qTIr5',  
+        userPoolClientId: '42j5ged229crnj8eq3ai6hqc5i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
