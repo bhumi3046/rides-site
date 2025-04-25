b248651d0594e229204c1beb5cdd4ba5bec7ec19
bhumi3046 <143391198+bhumi3046@users.noreply.github.com>
2025-04-26 00:03:14 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_grntrLaBA',  
         userPoolClientId: 'q1nbtms2fridau822hh6vc08s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientsecret: 'a6f6uu05mm1gff5vc3kruilbluq9gqq467b3flipbkhuv9nm94r'
         region: 'us-east-1'
     },
     api: {
