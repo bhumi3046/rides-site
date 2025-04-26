2dc84c68b92b6651aa0a526c99460a07e4430e1c
bhumi3046 <143391198+bhumi3046@users.noreply.github.com>
2025-04-26 12:14:37 +0530

    Update ride.js

--- js/ride.js
+++ js/ride.js
@@ -5,7 +5,6 @@ WildRydes.map = WildRydes.map || {};
 
 (function rideScopeWrapper($) {
     var authToken;
-
     WildRydes.authToken.then(function setAuthToken(token) {
         if (token) {
             authToken = token;
@@ -16,8 +15,6 @@ WildRydes.map = WildRydes.map || {};
         alert(error);
         window.location.href = '/signin.html';
     });
-
-    // Request Unicorn function when a pickup location is selected
     function requestUnicorn(pickupLocation) {
         $.ajax({
             method: 'POST',
@@ -36,7 +33,7 @@ WildRydes.map = WildRydes.map || {};
             error: function ajaxError(jqXHR, textStatus, errorThrown) {
                 console.error('Error requesting ride: ', textStatus, ', Details: ', errorThrown);
                 console.error('Response: ', jqXHR.responseText);
-                alert('An error occurred when requesting your unicorn:\n' + jqXHR.responseText);
+                alert('An error occured when requesting your unicorn:\n' + jqXHR.responseText);
             }
         });
     }
@@ -85,77 +82,26 @@ WildRydes.map = WildRydes.map || {};
         requestUnicorn(pickupLocation);
     }
 
-    // Initialize the map using ArcGIS
-    require([
-        "esri/Map",
-        "esri/views/MapView",
-        "esri/Graphic",
-        "esri/geometry/Point",
-        "esri/symbols/SimpleMarkerSymbol",
-        "esri/geometry/SpatialReference",
-        "dojo/domReady!"
-    ], function (Map, MapView, Graphic, Point, SimpleMarkerSymbol, SpatialReference) {
+    function animateArrival(callback) {
+        var dest = WildRydes.map.selectedPoint;
+        var origin = {};
 
-        // Coordinates for Greater Noida
-        var center = { lat: 28.4573787, lng: 77.52754575 };
+        if (dest.latitude > WildRydes.map.center.latitude) {
+            origin.latitude = WildRydes.map.extent.minLat;
+        } else {
+            origin.latitude = WildRydes.map.extent.maxLat;
+        }
 
-        // Create the map
-        var map = new Map({
-            basemap: "streets-navigation-vector"  // Choose your preferred basemap here
-        });
+        if (dest.longitude > WildRydes.map.center.longitude) {
+            origin.longitude = WildRydes.map.extent.minLng;
+        } else {
+            origin.longitude = WildRydes.map.extent.maxLng;
+        }
 
-        // Create the map view
-        var view = new MapView({
-            container: "mapView", // ID of the container div
-            map: map,
-            center: [center.lng, center.lat], // Set center of map to Greater Noida
-            zoom: 14
-        });
+        WildRydes.map.animate(origin, dest, callback);
+    }
 
-        // Create a marker symbol for the pickup location
-        var markerSymbol = new SimpleMarkerSymbol({
-            color: [226, 119, 40], // Orange color
-            size: 10,
-            outline: {
-                color: [255, 255, 255],
-                width: 1
-            }
-        });
-
-        // Event listener for click event on the map
-        view.on("click", function (event) {
-            var point = new Point({
-                longitude: event.mapPoint.longitude,
-                latitude: event.mapPoint.latitude,
-                spatialReference: new SpatialReference({ wkid: 4326 })
-            });
-
-            // Create a graphic (marker) at the clicked point
-            var pointGraphic = new Graphic({
-                geometry: point,
-                symbol: markerSymbol
-            });
-
-            // Remove any previous graphics and add the new one
-            view.graphics.removeAll();
-            view.graphics.add(pointGraphic);
-
-            // Save the location for pickup
-            WildRydes.map.selectedPoint = {
-                latitude: point.latitude,
-                longitude: point.longitude
-            };
-
-            // Enable the request button
-            $('#request').prop('disabled', false);
-            $('#request').text('Request Unicorn');
-        });
-
-    });
-
-    // Display updates
     function displayUpdate(text) {
         $('#updates').append($('<li>' + text + '</li>'));
     }
-
 }(jQuery));
